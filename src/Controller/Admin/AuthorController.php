4d87c23123087aa707603c5dfa15ff45eb637963
luljn <mbohlulajonathan4@gmail.com>
2024-08-06 23:25:29 +0200

    Add author form page added

--- src/Controller/Admin/AuthorController.php
+++ src/Controller/Admin/AuthorController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Author;
+use App\Form\AuthorType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/admin/author')]
 class AuthorController extends AbstractController
 {
-    #[Route('/admin/author', name: 'app_admin_author')]
+    #[Route('', name: 'app_admin_author')]
     public function index(): Response
     {
         return $this->render('admin/author/index.html.twig', [
             'controller_name' => 'AuthorController',
+        ]);
+    }
+
+    #[Route('/new', name: 'app_admin_author_new', methods: ['GET'])]
+    public function new(): Response
+    {
+        $author = new Author();
+        $form = $this->createForm(AuthorType::class, $author);
+        
+        return $this->render('admin/author/new.html.twig', [
+            'form' => $form,
         ]);
     }
 }
--- src/Form/AuthorType.php
+++ src/Form/AuthorType.php
@@ -4,10 +4,10 @@ namespace App\Form;
 
 use App\Entity\Author;
 use App\Entity\Book;
-use Doctrine\DBAL\Types\DateType;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
