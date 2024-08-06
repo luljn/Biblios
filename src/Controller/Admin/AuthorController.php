da16fb4950813a69d71111b695fcc7a4b346f46e
luljn <mbohlulajonathan4@gmail.com>
2024-08-06 23:49:31 +0200

    Author form upgraded to send data

--- src/Controller/Admin/AuthorController.php
+++ src/Controller/Admin/AuthorController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Author;
 use App\Form\AuthorType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -19,11 +20,16 @@ class AuthorController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_admin_author_new', methods: ['GET'])]
-    public function new(): Response
+    #[Route('/new', name: 'app_admin_author_new', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
     {
         $author = new Author();
         $form = $this->createForm(AuthorType::class, $author);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+        
+        }
         
         return $this->render('admin/author/new.html.twig', [
             'form' => $form,
