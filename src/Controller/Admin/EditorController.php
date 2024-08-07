1d9d0b78905f884932cbc8449a7aa27d1e048017
luljn <mbohlulajonathan4@gmail.com>
2024-08-07 20:32:27 +0200

    EditorType Form added

---
+++ src/Controller/Admin/EditorController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin/editor')]
+class EditorController extends AbstractController
+{
+    #[Route('', name: 'app_admin_editor')]
+    public function index(): Response
+    {
+        return $this->render('admin/editor/index.html.twig', [
+            'controller_name' => 'EditorController',
+        ]);
+    }
+
+    #[Route('/new', name: 'app_admin_editor_new', methods: ['GET', 'POST'])]
+    public function new(): Response 
+    {
+        return $this->render('admin/editor/new.html.twig');
+    }
+}
