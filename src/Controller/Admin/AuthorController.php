a52c168a7f0f8d5e88345650e22c26505d95b741
luljn <mbohlulajonathan4@gmail.com>
2024-09-01 20:43:42 +0200

    Update informatins about an author feature added

--- src/Controller/Admin/AuthorController.php
+++ src/Controller/Admin/AuthorController.php
@@ -45,9 +45,10 @@ class AuthorController extends AbstractController
     }
 
     #[Route('/new', name: 'app_admin_author_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $manager): Response
+    #[Route('/{id}/edit', name: 'app_admin_author_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function new(?Author $author, Request $request, EntityManagerInterface $manager): Response
     {
-        $author = new Author();
+        $author ??= new Author();
         $form = $this->createForm(AuthorType::class, $author);
 
         $form->handleRequest($request);
@@ -56,7 +57,7 @@ class AuthorController extends AbstractController
             $manager->persist($author);
             $manager->flush();
 
-            return $this->redirectToRoute('app_admin_author_new');
+            return $this->redirectToRoute('app_admin_author_show', ['id' => $author->getId()]);
         }
         
         return $this->render('admin/author/new.html.twig', [
