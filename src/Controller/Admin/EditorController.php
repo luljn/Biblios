7c62ce4cf88b0bdac0334896bf0771733aaba0f5
luljn <mbohlulajonathan4@gmail.com>
2024-09-02 22:16:56 +0200

    Admin pages for editors added

--- src/Controller/Admin/EditorController.php
+++ src/Controller/Admin/EditorController.php
@@ -4,7 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Editor;
 use App\Form\EditorType;
+use App\Repository\EditorRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,17 +17,25 @@ use Symfony\Component\Routing\Attribute\Route;
 class EditorController extends AbstractController
 {
     #[Route('', name: 'app_admin_editor')]
-    public function index(): Response
+    public function index(Request $request, EditorRepository $repository): Response
     {
+        $editors = Pagerfanta::createForCurrentPageWithMaxPerPage(
+            new QueryAdapter($repository->createQueryBuilder('e')),
+            $request->query->get('page', default: 1),
+            maxPerPage: 10
+        );
+
         return $this->render('admin/editor/index.html.twig', [
             'controller_name' => 'EditorController',
+            'editors' => $editors,
         ]);
     }
 
     #[Route('/new', name: 'app_admin_editor_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $manager): Response 
+    #[Route('/{id}/edit', name: 'app_admin_editor_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function new(?Editor $editor, Request $request, EntityManagerInterface $manager): Response 
     {
-        $editor = new Editor();
+        $editor ??= new Editor();
         $form = $this->createForm(EditorType::class, $editor);
 
         $form->handleRequest($request);
@@ -33,11 +44,19 @@ class EditorController extends AbstractController
             $manager->persist($editor);
             $manager->flush();
 
-            return $this->redirectToRoute('app_admin_editor_new');
+            return $this->redirectToRoute('app_admin_editor_show', ['id' => $editor->getId()]);
         }
 
         return $this->render('admin/editor/new.html.twig', [
             'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_editor_show', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function show(?Editor $editor) : Response
+    {
+        return $this->render('admin/editor/show.html.twig', [
+            'editor' => $editor,
         ]);
     }
 }
