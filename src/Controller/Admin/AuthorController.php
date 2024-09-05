76598fa51701354e7fd0954af1d6c25be309373e
luljn <mbohlulajonathan4@gmail.com>
2024-09-05 21:27:56 +0200

    Users role hierarchy managed

--- src/Controller/Admin/AuthorController.php
+++ src/Controller/Admin/AuthorController.php
@@ -49,6 +49,10 @@ class AuthorController extends AbstractController
     #[Route('/{id}/edit', name: 'app_admin_author_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
     public function new(?Author $author, Request $request, EntityManagerInterface $manager): Response
     {
+        if(null == $author){
+
+            $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        }
         $author ??= new Author();
         $form = $this->createForm(AuthorType::class, $author);
 
