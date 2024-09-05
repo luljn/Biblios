a7ec7e552ffbe443e32909330fb161447880f6c2
luljn <mbohlulajonathan4@gmail.com>
2024-09-05 21:10:17 +0200

    Restriction on the list of authors in the admin zone added

--- src/Controller/Admin/AuthorController.php
+++ src/Controller/Admin/AuthorController.php
@@ -12,10 +12,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/admin/author')]
 class AuthorController extends AbstractController
 {
+    #[IsGranted('IS_AUTHENTICATED')]
     #[Route('', name: 'app_admin_author', methods : ['GET'])]
     public function index(Request $request, AuthorRepository $repository): Response
     {
